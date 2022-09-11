d0f5c65b669cf78109bd86beb2369ae4d7efcc92
diogo cordeiro <dhyhenriko@gmail.com>
2022-09-11 18:10:53 -0500

    refactor: add theme

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,10 +4,9 @@ import { Button} from '@mui/material';
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path='/pagina-inicial' element={<Button>Test</Button>}/>
+            <Route path='/pagina-inicial' element={<Button variant='contained' color='primary'>Test</Button>}/>
 
             <Route path='*' element={<Navigate to="/pagina-inicial" />}/>
-
         </Routes>
     );
 }
---
+++ src/shared/themes/light.ts
@@ -0,0 +1,25 @@
+import { createTheme} from '@mui/material';
+import { blue, cyan } from '@mui/material/colors';
+
+
+export const LightTheme = createTheme({
+    palette: {
+        primary: {
+            main:blue[700],
+            dark:blue[800],
+            light:blue[500] ,
+            contrastText:'#ffffff',
+        },
+        secondary:{
+            main: cyan[500],
+            dark: cyan[400],
+            light: cyan[300],
+            contrastText: '#ffffff',
+        },
+        background:{
+            default: '#ffffff',
+            paper: '#f7f6f3', 
+        }
+    }
+});
+
